a1140f4f8fcb0337a2fc2407eecae4e30428546f
Banerjee Aditya <aditya.banerjee@activate.sg>
2025-01-07 16:03:06 +0800

    add "hello world" endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let router: Router = Router::new().route("/", get(hello_world));
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let tcp_listener: TcpListener = TcpListener::bind(addr).await.unwrap();
+
+    println!("🚀 Server listening on {}", addr);
+    axum::serve(tcp_listener, router).await.unwrap();
 }
